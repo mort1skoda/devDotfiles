0775931243cf585f090713420fd6757391d5d2b2
mort1skoda <mort1skoda@gmail.com>
2022-03-21 20:37:09 +0100

    Updateed:

--- .pushDotfiles.sh
+++ .pushDotfiles.sh
@@ -4,7 +4,10 @@
 #git add .a* .b* .c* .g* .l* .p* .t* .v* .z*
 #git rm .viminfo .bash_history
 git add -u
-git commit -m "Update"
+update="Updateed: "
+datTim=$(date +'%y%m%d_%H%M')
+msg="$update$datTime"
+git commit -m "$msg"
 git push -u origin master
 git status
 
