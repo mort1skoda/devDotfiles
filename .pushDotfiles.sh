11b4c611a41b128e6cc3fac90265cdd0844e0ac5
mort1skoda <mort1skoda@gmail.com>
2022-03-21 19:55:10 +0100

    asdf

--- .pushDotfiles.sh
+++ .pushDotfiles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-git add README.md
-git add .a* .b* .c* .g* .l* .p* .t* .v* .z*
-git rm .viminfo .bash_history
+#git add README.md
+#git add .a* .b* .c* .g* .l* .p* .t* .v* .z*
+#git rm .viminfo .bash_history
+git add -u
 git commit -m "update"
 git push -u origin master
 git status
