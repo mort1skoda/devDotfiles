83b21a5d8b4cf48051fa6b8b843200c540ae8677
mort1skoda <mort1skoda@gmail.com>
2022-03-24 03:36:00 +0100

    Updated: 220324_0336

---
+++ .aliases.sh
@@ -0,0 +1,27 @@
+alias l='ls -la --color'
+alias m='make'
+alias ..='cd ..'
+
+alias v='vim'
+alias f='vifm ~ ~/dev'
+alias tl='tmux ls'
+alias ta='tmux a -t 0'
+
+alias sai='sudo apt install'
+alias sau='sudo apt update && sudo apt upgrade -y && sudo apt autoremove'
+
+alias va='vim ~/.aliases.sh && source ~/.bashrc'
+alias vb='vim ~/.bashrc && source ~/.bashrc'
+alias .b='source ~/.bashrc'
+
+alias vz='vim .zshrc && source ~/.zshrc'
+alias .z='source ~/.zshrc'
+
+alias vv='vim ~/.vimrc'
+alias vf='vim ~/.vifm/vifmrc.vim'
+alias vt='vim ~/.tmux.conf'
+
+alias e='exit'
+#alias a='clear && alias'
+alias gp='./.push*'
+
