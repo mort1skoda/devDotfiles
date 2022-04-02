399dccd4b1314c0f496c3ace94321899bec7b020
mort1skoda <mort1skoda@gmail.com>
2022-04-02 18:59:32 +0200

    Updated: 220402_1859

--- .gitPushDotfiles.sh
+++ .gitPushDotfiles.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#git add README.md
-git add .
-#git rm .viminfo .bash_history
-git add -u
+git add --all
+git status
 update="Updated: "
 datTim=$(date +'%y%m%d_%H%M')
 msg="$update$datTim"
 echo $msg
-#exit
 
 git commit -m "$msg"
 git push -u origin master
