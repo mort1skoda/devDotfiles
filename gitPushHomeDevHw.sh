67dec27523047d01041547bb56c7b18db3b8c7ef
mort1skoda <mort1skoda@gmail.com>
2023-01-13 10:27:17 +0100

    modified:   gitPushHomeDevHw.sh

--- gitPushHomeDevHw.sh
+++ gitPushHomeDevHw.sh
@@ -32,6 +32,8 @@ git push -u origin master
 
 #################################
 create a new repository on the command line
+NB! first you must create a empty repo online.
+(NO README.md)  README.md is created on the command line.
 
 echo "# test" >> README.md
 git init
